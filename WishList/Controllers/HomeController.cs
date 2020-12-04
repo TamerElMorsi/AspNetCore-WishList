527bb43294b3f8aa687b99d9fbd86192983d493d
TamerElMorsi <tamer.elmorsi@gmail.com>
2020-12-04 11:22:11 +0200

    Update HomeController.cs

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,7 +10,12 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return Index();
+        }
+
+        public IActionResult Error()
+        {
+            return Error();
         }
     }
 }
